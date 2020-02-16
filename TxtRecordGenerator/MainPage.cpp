7ef0944be888008676a515c2bd773a493eb62772
梁建溢 <liangjianyi.com@outlook.com>
2020-02-16 08:30:19 -0600

    Changesets-44

--- TxtRecordGenerator/MainPage.cpp
+++ TxtRecordGenerator/MainPage.cpp
@@ -16,15 +16,24 @@ namespace winrt::TxtRecordGenerator::implementation
 }
 
 
-void winrt::TxtRecordGenerator::implementation::MainPage::GeneratorButton_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::UI::Xaml::RoutedEventArgs const& e) {
+void winrt::TxtRecordGenerator::implementation::MainPage::GeneratorButton_Click(winrt::Windows::Foundation::IInspectable const&, winrt::Windows::UI::Xaml::RoutedEventArgs const&) {
 	IReference<DateTime> beginDateRef = BeginDatePicker().Date();
 	IReference<DateTime> endDateRef = EndDatePicker().Date();
 	if (beginDateRef != nullptr && endDateRef != nullptr) {
-		const time_t& bt = beginDateRef.Value().time_since_epoch().count();
-		const time_t& et = endDateRef.Value().time_since_epoch().count();
+		// DateTimePick 返回的时钟震荡周期很奇怪，与std::chrono::system_clock::now()的周期大约相差 11644473596UL，
+		// 目前原因尚不清楚。
+		// 通过乘以 std::chrono::system_clock::period::num / std::chrono::system_clock::period::den  - 11644473596UL
+		// 转化为实际的秒数
+		time_t bt = beginDateRef.Value().time_since_epoch().count() * std::chrono::system_clock::period::num / std::chrono::system_clock::period::den;
+		bt = static_cast<time_t>(static_cast<uint64_t>(bt) - 11644473596UL);
+		time_t et = endDateRef.Value().time_since_epoch().count() * std::chrono::system_clock::period::num / std::chrono::system_clock::period::den;
+		et = static_cast<time_t>(static_cast<uint64_t>(et) - 11644473596UL);
+
 		const Janyee::DateTime& beginDateTime = Janyee::DateTime(bt);
 		const Janyee::DateTime& endDateTime = Janyee::DateTime(et);
-		const uint32_t ENTRIES_COUNT = 100;
+		OutputDebugStringA(("beginDateTime: " + beginDateTime.ToString() + "\n").c_str());
+		OutputDebugStringA(("endDateTime: " + endDateTime.ToString() + "\n").c_str());
+		const uint32_t ENTRIES_COUNT = 10;
 		for (uint32_t i = 0; i < ENTRIES_COUNT; i++) {
 			const auto& dateTuple = Janyee::Utilty::RandomDate(
 				std::make_tuple(beginDateTime.GetYear(), endDateTime.GetYear()),
@@ -32,10 +41,10 @@ void winrt::TxtRecordGenerator::implementation::MainPage::GeneratorButton_Click(
 				std::make_tuple(beginDateTime.GetDayOfMonth(), endDateTime.GetDayOfMonth())
 			);
 			const Janyee::DateTime& date = Janyee::DateTime(std::get<0>(dateTuple), std::get<1>(dateTuple), std::get<2>(dateTuple));
-			OutputDebugStringA(date.ToString().c_str());
+			OutputDebugStringA((date.ToString() + "\n").c_str());
 		}
 	}
 	else {
-		OutputDebugStringA("You not pick a date.");
+		OutputDebugStringA("You not pick a date.\n");
 	}
 }
--- TxtRecordGenerator/MainPage.h
+++ TxtRecordGenerator/MainPage.h
@@ -8,6 +8,10 @@ namespace winrt::TxtRecordGenerator::implementation
     {
         MainPage();
 		void GeneratorButton_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::UI::Xaml::RoutedEventArgs const& e);
+		void BeginDatePicker_Opened(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::Foundation::IInspectable const& e);
+		void BeginDatePicker_DateChanged(winrt::Windows::UI::Xaml::Controls::CalendarDatePicker const& sender, winrt::Windows::UI::Xaml::Controls::CalendarDatePickerDateChangedEventArgs const& args);
+		void BeginDatePicker_Closed(winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::Foundation::IInspectable const& e);
+		void BeginDatePicker_CalendarViewDayItemChanging(winrt::Windows::UI::Xaml::Controls::CalendarView const& sender, winrt::Windows::UI::Xaml::Controls::CalendarViewDayItemChangingEventArgs const& e);
 	};
 }
 
